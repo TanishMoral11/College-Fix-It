ded01d56d51e9c226c301c07fd2aed5db7bd8638
TanishMoral11 <ishantmoral@gmail.com>
2026-02-04 15:07:47 +0530

    lit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,13 +33,25 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
-    buildFeatures{
+    
+    kapt {
+        javacOptions {
+            option("-source", "11")
+            option("-target", "11")
+        }
+        arguments {
+            arg("databinding.enabledForTests", "true")
+            arg("databinding.disableSafeViewModelAccess", "false")
+        }
+    }
+    
+    buildFeatures {
         viewBinding = true
         dataBinding = true
     }
